e253b3ef48dafd2546edc7d3b0571145f1318de2
Sharanya Ram <artsharanya@gmail.com>
2026-01-19 09:12:13 -0500

    new changes in images-gallery

---
+++ images-gallery/frontend/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navbar} from 'react-bootstrap';
+
+const Header = ({title}) => {
+    return (
+
+         <Navbar bg="light" data-bs-theme="light">
+         <Navbar.Brand href="/">{title}</Navbar.Brand>
+
+        </Navbar>
+    )
+};
+
+export default Header;
