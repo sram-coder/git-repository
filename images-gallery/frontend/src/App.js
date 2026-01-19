55c6dfdb1825e5fa7b4b183b0c5601bae8ebab68
Sharanya Ram <artsharanya@gmail.com>
2026-01-19 12:56:29 -0500

    env modified

--- images-gallery/frontend/src/App.js
+++ images-gallery/frontend/src/App.js
@@ -1,30 +1,29 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
-
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
-const App = () =>{
-  const [word, setWord] = useState('')
+const App = () => {
+  const [word, setWord] = useState('');
 
   const handleSearchSubmit = (e) => {
-  e.preventDefault();
-  console.log(word)
-  fetch(`https://api.unsplash.com/photos/random/?query=${encodeURIComponent(word)}&client_id=${UNSPLASH_KEY}`)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-  })
-  .catch(err => console.log(err));
+    e.preventDefault();
+    console.log(word);
+
+    fetch(`https://api.unsplash.com/photos/random?query=${encodeURIComponent(word)}&client_id=${UNSPLASH_KEY}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => console.log(err));
   }
 
-  
   return (
     <div>
-      <Header title = "Images Gallery"/>
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
