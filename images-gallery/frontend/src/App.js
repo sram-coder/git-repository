b386dd07bf75432653359c096e50fbcc87f93297
Sharanya Ram <artsharanya@gmail.com>
2026-01-19 12:24:58 -0500

    new url

--- images-gallery/frontend/src/App.js
+++ images-gallery/frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Search from './components/Search';
 
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () =>{
   const [word, setWord] = useState('')
@@ -11,14 +12,19 @@ const App = () =>{
   const handleSearchSubmit = (e) => {
   e.preventDefault();
   console.log(word)
+  fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+  })
+  .catch(err => console.log(err));
   }
 
-  console.log(process.env.REACT_APP_URL)
   
   return (
     <div>
       <Header title = "Images Gallery"/>
-      <Search word={word} setWord={setWord}handleSearchSubmit={handleSearchSubmit}/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
