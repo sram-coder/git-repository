b0e30fd6bd6b7a5f67f80addf802bc2d46680110
Sharanya Ram <artsharanya@gmail.com>
2026-01-20 12:55:53 -0500

    save images in state

--- images-gallery/frontend/src/App.js
+++ images-gallery/frontend/src/App.js
@@ -1,25 +1,26 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "../components/Header";
-import Search from "../components/Search";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
-console.log("Unsplash Key:", UNSPLASH_KEY);
-
 const App = () => {
   const [word, setWord] = useState("");
+  const [images, setImages] = useState([]);
+
+  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-
     fetch(
       `https://api.unsplash.com/photos/random?query=${encodeURIComponent(word)}&client_id=${UNSPLASH_KEY}`,
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images]);
+        console.log(images);
       })
       .catch((err) => console.log(err));
 
