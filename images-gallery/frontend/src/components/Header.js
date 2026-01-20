a104ce0c2886e803ef4f9f42864f8d5d8cef3e40
Sharanya Ram <artsharanya@gmail.com>
2026-01-20 17:29:52 -0500

    logo added

--- images-gallery/frontend/src/components/Header.js
+++ images-gallery/frontend/src/components/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
-
+import {ReactComponent as Logo} from '../images/logo.svg';
 const navbarStyle = {
-  backgroundColor: 'lightblue',
+  backgroundColor: 'efe0e0',
 };
 
 const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} data-bs-theme="light">
       <Container>
-        <Navbar.Brand href="/">{title}</Navbar.Brand>
+        <Logo style={{maxWidth:'20rem', maxHeight: '4rem'}}/>
       </Container>
     </Navbar>
   );
